081196b0cf35dbfad791f48cd2575e06e5cd5057
Angelica Hernandez <gel.lorraine@gmail.com>
2021-08-07 15:11:22 -0700

    Resume updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,17 +5,7 @@ export default function About() {
         <div className="pages container-fluid">
             <h2> About Info</h2>
             <p> I am a passionate Full Stack Web Developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. I am dedicated and eager to continue to learn, improve and update my skill set and problem solving techniques. I look forward to continuously gaining knowledge from my peers and work collaboratively with others while being able to contribute to the technology industry.</p>
-            <div className="flex-row flex-wrap">
-                <div data-iframe-width="300" data-iframe-height="290" data-share-badge-id="8390eb15-79a6-4436-abca-68dfcacf5931" data-share-badge-host="https://www.credly.com"></div>
-
-                <div data-iframe-width="300" data-iframe-height="290" data-share-badge-id="3fff4eba-e681-4c99-bfd1-7f45012f9d42" data-share-badge-host="https://www.credly.com"></div>
-
-                <div data-iframe-width="300" data-iframe-height="290" data-share-badge-id="d55aed9a-1084-4dc8-a1b7-750f45fbae4d" data-share-badge-host="https://www.credly.com"></div>
-
-                <div data-iframe-width="300" data-iframe-height="290" data-share-badge-id="86d00870-6146-42a1-8546-0e8ff30e69af" data-share-badge-host="https://www.credly.com"></div>
-
-            </div>
-        </div>
+        </div >
     );
 }
 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,6 +4,26 @@ export default function Resume() {
     return (
         <div className="pages container-fluid">
             <h2> Resume Info</h2>
+            <div className="d-flex flex-row flex-wrap justify-content-center">
+
+                <div> <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/250x250/images/109b65a3-b8c5-4802-95a4-3db2f1e5ef3a/6f5fb490753026eb192a66509fe7332c.png" width="200" alt="" height="200"></img></div>
+
+                <div>
+                    <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/200x200/images/8358353b-3c24-4b79-9fea-ff9c2c78f7d6/cac05207c7217149aaff5418f4cc94de.png" width="200" alt="" height="200"></img>
+                </div>
+
+                <div>
+                    <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/200x200/images/044c920a-57fa-488d-a70e-fc8d39de4bec/0e6480912dcc88d3ca1c400869a97034.png" width="200" alt="" height="200"></img>
+                </div>
+
+                <div>
+                    <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/200x200/images/f10424f9-0222-4611-8001-2fbdb9bf8471/d22b12279a8399e20d5722f5b15a0340.png" width="200" alt="" height="200"></img>
+                </div>
+
+
+
+            </div>
         </div>
+
     );
 }
