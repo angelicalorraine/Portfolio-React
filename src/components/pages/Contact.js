f7841c66b55b969e92010d50241a6e1fa2e25905
Angelica Hernandez <gel.lorraine@gmail.com>
2021-08-07 16:53:06 -0700

    Added homework assignments

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,97 @@
 import React from 'react';
+import { useState } from 'react';
+import { checkPassword, validateEmail } from '../utils/helpers';
 
 export default function Contact() {
+
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'userName') {
+            setUserName(inputValue);
+        } else {
+            setPassword(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+
+        e.preventDefault();
+
+        if (!validateEmail(email) || !userName) {
+            setErrorMessage('Email or username is invalid');
+
+            return;
+
+        }
+        if (!checkPassword(password)) {
+            setErrorMessage(
+                `Choose a more secure password for the account: ${userName}`
+            );
+            return;
+        }
+        alert(`Hello ${userName}`);
+
+        setUserName('');
+        setPassword('');
+        setEmail('');
+    };
+
     return (
-        <div className="pages container-fluid">
+        <div className="pages container-fluid d-flex justify-content-center flex-column">
             <h2> Contact Info</h2>
+            <div className="form col-md-6 shadow-lg pt-4 m-5">
+                <form className="p-3 mr-2">
+                    <div className="form-group">
+
+                        <div className="form-row mb-2">
+
+                            <input
+                                className="input-group form-control"
+                                value={userName}
+                                name="userName"
+                                onChange={handleInputChange}
+                                type="text"
+                                placeholder="Name"
+                            />
+
+                        </div>
+                        <div className="form-row mb-2">
+                            <input
+                                className="input-group form-control"
+                                value={email}
+                                name="email"
+                                onChange={handleInputChange}
+                                type="email"
+                                placeholder="Email"
+                            />
+                        </div>
+
+                        <div className="form-row mb-2">
+                            <div className="input-group">
+                                <textarea className="form-control" placeholder="Message" aria-label="With textarea"></textarea>
+                            </div>
+                        </div>
+
+                        <button className=" btn btn-outline-info" type="button" onClick={handleFormSubmit}>Submit</button>
+                    </div>
+                </form>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -23,13 +23,11 @@ export default function Project() {
                                     <p className="card-text ">
                                         Our knowledge and newly learned skills were put to the test by
                                         creating a web application from scratch. We conceived and
-                                        executed a design and integrated data received from multiple
-                                    server-side API requests.</p>
+                                        executed a design and integrated data received from multiple server-side API requests.</p>
                                     <p className="card-text">
                                         Study Buddy was built with the developer in mind, we are
                                         committed to being lifelong students. When searching a
-                                        question or topic and the results will show you only resolved
-                                    results from StackOverflow along with suggested tutorials!{" "}
+                                        question or topic and the results will show you only resolved results from StackOverflow along with suggested tutorials!
                                     </p>
                                     <a href="https://github.com/angelicalorraine/Study_Buddy"
                                         target="_blank"
@@ -61,16 +59,69 @@ export default function Project() {
                         </div>
                     </div>
 
+                </div>
+            </div>
 
 
 
+            <div className="container d-flex justify-content-center ">
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+
+
+                    <div className="col">
+                        <div className="card h-100 align-items-center ">
+                            <img src="Images/ForeWeather (1).png" className="card-img-top img-thumbnail" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Fore-Weather</h5>
+                                <p className="card-text">This homeworks challenge was to build a weather dashboard that will run in the browser
+                                    and feature dynamically updated HTML and CSS. The main aspect of this assignment was accessing a Server-side API to retrieve data, which was OpenWeather API</p>
+                                <a href="https://github.com/angelicalorraine/Fore-Weather" target="_blank"
+                                    className="btn btn-outline-secondary">Repository</a>
+                                <a href="https://angelicalorraine.github.io/Fore-Weather/" target="_blank" className="btn btn-outline-info">Launch App</a>
+                            </div>
+                        </div>
+                    </div>
 
 
 
+                    <div className="col">
+                        <div className="card h-100 align-items-center">
+                            <img src="Images/InQUIZition.png" className="card-img-top img-thumbnail" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">In-QUIZ-ition</h5>
+                                <p className="card-text">This assignment was to build a timed coding quiz with multiple-choice questions. This app
+                                    will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code. Also saving your score and initials in the local storage.</p>
+                                <a href="https://github.com/angelicalorraine/In-QUIZ-ition" target="_blank"
+                                    className="btn btn-outline-secondary">Repository</a>
+                                <a href="https://angelicalorraine.github.io/In-QUIZ-ition/" target="_blank"
+                                    className="btn btn-outline-info">Launch App</a>
+                            </div>
+                        </div>
+                    </div>
 
+
+                    <div className="col">
+                        <div className="card h-100 align-items-center">
+                            <img src="Images/daily-sched.png" className="card-img-top img-thumbnail" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Daily Agenda</h5>
+                                <p className="card-text">This is a creation of a simple calendar application that allows a user to save events for
+                                    each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.</p>
+                                <a href="https://github.com/angelicalorraine/Angelicas-Agenda" target="_blank"
+                                    className="btn btn-outline-secondary">Repository</a>
+                                <a href="https://angelicalorraine.github.io/Angelicas-Agenda/" target="_blank"
+                                    className="btn btn-outline-info">Launch App</a>
+                            </div>
+                        </div>
+                    </div>
 
 
                 </div>
+
+
+
+
+
             </div>
         </div>
     );
