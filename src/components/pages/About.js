ce52a3d068ea0cc4eaf5736db4b679ed952d4d69
Angelica Hernandez <gel.lorraine@gmail.com>
2021-08-11 02:30:00 -0700

    about me updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,12 +8,31 @@ export default function About() {
 
 
 
-            <div className="d-flex flex-row justify-content-center">
-                <div className="d-flex col-md-4 justify-content-end" > <img className="gel" src={gel} ></img></div>
+            <div className="d-flex flex-wrap justify-content-center ">
+                <div className="leftmain flex-shrink-1" > <img className="gel" src={gel} ></img></div>
 
-                <div className="d-flex m-3 col-md-8 flex-column about" >
+                <div className="rightmain flex-shrink-1 mt-5" >
 
                     <p className="aboutme"> I am a passionate Full Stack Web Developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. I am dedicated and eager to continue to learn, improve and update my skill set and problem solving techniques. I look forward to continuously gaining knowledge from my peers and work collaboratively with others while being able to contribute to the technology industry.</p>
+                    <div className="d-flex flex-row flex-wrap justify-content-center">
+
+                        <div> <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/250x250/images/109b65a3-b8c5-4802-95a4-3db2f1e5ef3a/6f5fb490753026eb192a66509fe7332c.png" width="200" alt="" height="200"></img></div>
+
+                        <div>
+                            <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/200x200/images/8358353b-3c24-4b79-9fea-ff9c2c78f7d6/cac05207c7217149aaff5418f4cc94de.png" width="200" alt="" height="200"></img>
+                        </div>
+
+                        <div>
+                            <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/200x200/images/044c920a-57fa-488d-a70e-fc8d39de4bec/0e6480912dcc88d3ca1c400869a97034.png" width="200" alt="" height="200"></img>
+                        </div>
+
+                        <div>
+                            <img className="cr-standard-grid-item-content__image" src="https://images.credly.com/size/200x200/images/f10424f9-0222-4611-8001-2fbdb9bf8471/d22b12279a8399e20d5722f5b15a0340.png" width="200" alt="" height="200"></img>
+                        </div>
+
+
+
+                    </div>
                 </div>
 
 
